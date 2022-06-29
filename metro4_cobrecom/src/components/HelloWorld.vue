9af709a803fc4a8795a80ceac00847bb4feb24e9
ferilva1989 <filipefs.1989@gmail.com>
2022-06-29 14:37:33 -0300

    Certinho!

--- metro4_cobrecom/src/components/HelloWorld.vue
+++ metro4_cobrecom/src/components/HelloWorld.vue
@@ -8,13 +8,59 @@ defineProps({
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
-    </h3>
+  <div class="container-fluid start-screen h-100">
+    <div class="tiles-area clear">
+      <div class="tiles-grid tiles-group size-7 fg-white">
+        <div data-role="tile" data-size="wide" class="bg-cyan fg-white selected">
+          <span class="mif-power icon"></span>
+          <span class="branding-bar">INÍCIO</span>
+        </div>
+        <div data-role="tile" data-size="wide" class="bg-cyan fg-white">
+          <span class="mif-shrink icon"></span>
+          <span class="branding-bar">CARGA</span>
+        </div>
+        <div data-role="tile" data-size="wide" class="bg-cyan fg-white">
+          <span class="mif-history icon"></span>
+          <span class="branding-bar">REPOSIÇÃO</span>
+        </div>
+        <div data-role="tile" class="bg-cyan fg-white">
+          <span class="mif-backspace icon"></span>
+          <span class="branding-bar">DEVOLUÇÃO</span>
+        </div>
+        <div data-role="tile" data-size="wide" class="bg-cyan fg-white">
+          <span class="mif-list icon"></span>
+          <span class="branding-bar">REPORTE</span>
+        </div>
+        <div data-role="tile" class="bg-cyan fg-white">
+          <span class="mif-move-down icon"></span>
+          <span class="branding-bar">DESCARGA</span>
+        </div>
+        <div data-role="tile" data-size="wide" class="bg-cyan fg-white">
+          <span class="mif-sort-asc icon"></span>
+          <span class="branding-bar">RECLASSIFICAÇÃO</span>
+        </div>
+        <div data-role="tile" data-size="wide" class="bg-cyan fg-white">
+          <span class="mif-printer icon"></span>
+          <span class="branding-bar">REIMPRESSÃO</span>
+        </div>
+        <div data-role="tile" class="bg-cyan fg-white">
+          <span class="mif-clipboard icon"></span>
+          <span class="branding-bar">BAIXA</span>
+        </div>
+        <div data-role="tile" data-size="wide" class="bg-cyan fg-white">
+          <span class="mif-equalizer icon"></span>
+          <span class="branding-bar">PROGRAMAÇÃO</span>
+        </div>
+        <div data-role="tile" data-size="wide" class="bg-cyan fg-white">
+          <span class="mif-cross icon"></span>
+          <span class="branding-bar">INTERRUPÇÃO</span>
+        </div>
+        <div data-role="tile" data-size="wide" class="bg-cyan fg-white">
+          <span class="mif-infinite icon"></span>
+          <span class="branding-bar">MOVIMENTAÇÃO</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,6 +81,7 @@ h3 {
 }
 
 @media (min-width: 1024px) {
+
   .greetings h1,
   .greetings h3 {
     text-align: left;
