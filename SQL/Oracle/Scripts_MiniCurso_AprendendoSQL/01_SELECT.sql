b5a185d314aaf3cf196d52606a8736977e1d444b
ferilva1989 <filipefs.1989@gmail.com>
2022-07-20 14:28:02 -0300

    ajuste pastas

--- SQL/Oracle/Scripts_MiniCurso_AprendendoSQL/01_SELECT.sql
+++
@@ -1,3 +0,0 @@
--- Recuperando todos os dados da tabela EMPLOYEES
-SELECT  *
-FROM    HR.EMPLOYEES;
--- SQL/Oracle/Scripts_MiniCurso_AprendendoSQL/09_DESC.sql
+++
@@ -1,5 +0,0 @@
--- veja a estrutura da tabela EMPLOYEES
-DESC HR.EMPLOYEES;
-
--- veja a estrutura da tabela DEPARTMENTS
-DESCRIBE HR.DEPARTMENTS;
